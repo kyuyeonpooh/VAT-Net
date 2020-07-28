55bf0112851c71fa370680c0f0c05e2b9d9055e1
Kyuyeon <kyuyeonpooh@gmail.com>
2020-07-28 22:10:27 +0900

    Add visual and audio network

---
+++ models/TrimodalNet.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from models import AudioNet, VisualNet
+
+
+class TrimodalNet(nn.Module):
+    def __init__(self):
+        super(TrimodalNet, self).__init__()
+
+        self.visnet = VisualNet.VisualNet()  # (B, 512)
+        self.vis_head = nn.Sequential(
+            nn.Linear(512, 256),  # (B, 256)
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 256),  # (B, 256)
+            nn.BatchNorm1d(256)
+        )
+
+        self.audnet = AudioNet.AudioNet()
+        self.aud_head = nn.Sequential(
+            nn.Linear(512, 256)
+        )
+
+        self.fc1_va = nn.Linear(256, 128)
+        self.relu_va = nn.ReLU(inplace=True)
+        self.fc2_va = nn.Linear(128, 1)
+        self.sigmoid_va = nn.Sigmoid()
+
+        self.apply(self.init_bn)
+
+    def init_bn(self, m):
+        if isinstance(m, nn.BatchNorm1d):
+            nn.init.ones_(m.weight)
+            nn.init.zeros_(m.bias)            
+
+    def forward(self, img, aud):
+        v = self.visnet(img)
+        v = self.vis_head(v)
+        z_v = F.normalize(v, p=2, dim=1)
+
+        a = self.audnet(aud)
+        a = self.aud_head(a)
+        z_a = F.normalize(a, p=2, dim=1)
+
+        z_va = z_v * z_a
+        z_va = self.relu_va(self.fc1_va(z_va))
+        out_va = self.sigmoid_va(self.fc2_va(z_va))
+
+        return out_va.squeeze()
---
+++ models/VisualNet.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+
+class VisualNet(nn.Module):
+    """ VisualNet is following ResNet18 architecture.
+    """
+    def __init__(self):
+        super(VisualNet, self).__init__()
+        
+        self.resnet18 = models.resnet18(pretrained=False)
+        self.resnet18.fc = nn.Identity()  # Discard last fc layer
+
+    def forward(self, x):
+        return self.resnet18(x)  # (B, 512)
+
+
+if __name__ == "__main__":
+    # Model summary
+    model = VisualNet()
+    print(model)
+
+    # Forward passing test
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    x = torch.rand((128, 3, 224, 224)).to(device)
+    y = model(x)
+    print("Output shape: {}".format(y.shape))
+    print("Forward passing test succeeded.")
+
+    # Backward propgation test
+    y_gt = torch.rand((128, 512)).to(device)
+    loss = nn.MSELoss()(y, y_gt)
+    loss.backward()
+    print("Backward propagation test succeeded.")
+
+    # Number of parameters
+    print(
+        "Number of parameters:",
+        sum(p.numel() for p in model.parameters() if p.requires_grad),
+    )
